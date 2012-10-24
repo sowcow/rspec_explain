ad94e35e29429a0fe6555155285c3b27d6f9b658
Alexander K <xpyro@ya.ru>
2012-10-24 15:44:08 +0400

    more usages

--- lib/rspec_explain/explain.rb
+++ lib/rspec_explain/explain.rb
@@ -1,15 +1,33 @@
 module RspecExplain
   def explain method, examples, run='subject.%s(%s)'
 
-    examples = examples.each_slice(2)
+    examples = prepare examples
 
     context "##{method}" do
       examples.each do |input,output|
         specify "#{input} => #{output}" do
-          eval run % [method, input]  #subject.send(:method, input)
+          eval(run % [method, input.inspect]) #subject.send(:method, input)
         end
       end
     end
 
+  end
+
+  private
+  def prepare table
+    case table
+    when Array then table.map { |x| prepare_one x }.each_slice(2)
+    when String then prepare table.lines.map { |line| line.split '=>' }.flatten
+    else
+      raise
+    end
+  end
+
+  def prepare_one element
+    case element
+    when String then element.strip
+    else
+      element
+    end      
   end
 end
--- spec/explain_spec.rb
+++ spec/explain_spec.rb
@@ -3,12 +3,60 @@ require 'spec_helper'
 class Usage
   include RspecExplain
 
-  def square x  # the method we test
+  def square x  # first method we test
     x**2
   end
 
+  def no_spaces x  # second method we test
+    x.gsub(' ','_')
+  end  
+
+  def level_up x  # third method we test
+    x[%r|^(/.+/)[^/]+/$|]
+  end 
+
+
   def usage_1
     explain :square, [2,4,  3,9,  5,25,  10,100]  # 4 examples
+  end
+
+  def usage_2
+    explain :square, %w[
+      2 4
+      3 9
+      5 25
+      6 36
+      7 49
+    ].map(&:to_i)
+  end
+
+  def usage_3
+    explain :no_spaces, <<-EX
+      one string  => one_string
+      a b c       => a_b_c
+      a b  c   d  => a_b__c___d
+    EX
+  end  
+
+  def usage_4
+    explain :level_up, <<-EX
+      /x/y/z/ => /x/y/
+      /x/y/   => /x/
+      /one/   => /
+      /       => `nil`
+    EX
+               # `nil` will be evaluated
+  end
+
+
+  # for testing
+  def should_be_red
+   explain :level_up, <<-EX
+      /x/y/z/ => `true`
+      /x/y/   => `false`
+      /one/   => 123
+      /       => `2*2`
+    EX
   end
 
   private
@@ -47,8 +95,25 @@ describe RspecExplain do                                   #
 
     it 'invokes :method on subject' do
       subject.should_receive(:square).exactly(4).times
-      #subject.should_receive(:subject).exactly(4).times { subject }
       subject.usage_1
+    end
+
+    specify 'someday I will test `.should==output` part' do
+      pending
+    end
+
+
+    context 'obvious testing' do
+
+      it 'good examples should pass' do
+        subject.methods.grep(/usage_\d/).size.should be >= 4
+        subject.methods.grep(/usage_\d/).each { |method| subject.send method }
+      end
+
+      it 'bad examples should not pass' do
+        subject.should_be_red
+      end
+
     end
   end
 end
