1c90da1e09459de5272b06cf54715550efd24e71
Alexander K <xpyro@ya.ru>
2012-10-24 18:26:08 +0400

    moved

--- lib/rspec_explain.rb
+++ lib/rspec_explain.rb
@@ -1,5 +1,43 @@
-require "rspec_explain/version"
+require 'rspec_explain/version'
 
-module RspecExplain
-  # Your code goes here...
+RSpec.configure do |config|
+  config.extend RSpecExplain
+end
+
+module RSpecExplain
+  def explain method, examples, run='subject.%s(%s)'
+
+    examples = prepare examples
+
+    context "##{method}" do
+      examples.each do |input,output|
+        specify "#{input} => #{output}" do
+          eval(run % [method, input.inspect]).should == output #subject.send(:method, input)
+        end
+      end
+    end
+
+  end
+
+  private
+  def prepare table
+    case table
+    when Array then table.map { |x| prepare_one x }.each_slice(2)
+    when String then prepare table.lines.map { |line| line.split '=>' }.flatten
+    else
+      raise
+    end
+  end
+
+  COMMAND = %r"^`(.*)`$"
+  def prepare_one element
+    
+    element = element.strip if element.is_a? String
+
+    case element
+    when COMMAND then eval(element[COMMAND,1])
+    else
+      element
+    end      
+  end
 end
